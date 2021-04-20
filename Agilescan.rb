38041ddabdf6cd053694bc92f99bed1eca09bbc1
Chabi Yo, Sani <sani_chabi_yo@manulife.com>
2021-04-20 14:20:18 -0400

    update

--- Agilescan.rb
+++ Agilescan.rb
@@ -1,18 +1,18 @@
 require "FileUtils"
 
-class Infosec < Formula
+class Agilescan < Formula
   desc "CLI to deploy Frontend modules"
   homepage "https://github.com/schabiyo/infosec"
-  url "https://github.com/schabiyo/infosec/releases/download/v0.1.4/fed-cli-darwin-amd64-0.1.4.tar.gz"
+  url "https://github.com/schabiyo/infosec/releases/download/v0.1.4/agilescan-darwin-amd64-0.1.4.tar.gz"
   sha256 "c4bb34abd18ef743572c085fcbaf0a4d5c0770a7c889ae5ce422213bb35e80d4"
   head "https://github.com/schabiyo/infosec.git"
 
   def install
-    bin.install "infosec"
+    bin.install "agilescan"
   end
 
   # Homebrew requires tests.
   test do
-    assert_match "infosec-agent version 1.0.0", shell_output("#{bin}/infosec-agent -v", 2)
+    assert_match "agilescan version 1.0.0", shell_output("#{bin}/infosec-agent -v", 2)
   end
 end
