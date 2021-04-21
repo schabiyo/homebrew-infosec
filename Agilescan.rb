9fa397861f8b686a3bdaac9481acd11af18a603f
Chabi Yo, Sani <sani_chabi_yo@manulife.com>
2021-04-21 11:32:21 -0400

    update 2

--- Agilescan.rb
+++ Agilescan.rb
@@ -8,7 +8,7 @@ class Agilescan < Formula
   if !OS.linux?
     url "https://github.com/schabiyo/infosec/releases/download/v1/agilescan_macOs_64bit.zip"
     sha256 "9ee30fc358fae8d248a2d7538957089885da321dca3f09e3296fe2058e7ff74"
-    abort("Your Os  is not supported")
+    abort(" Sorry Your Os is not supported")
   end
   if OS.linux? && Hardware::CPU.intel?
     puts "Linux and CPU Intel".inspect
@@ -35,11 +35,13 @@ class Agilescan < Formula
 
   def install
     bin.install "agilescan"
+    system "tree", "--version"
     puts "Completed install"
   end
 
   def post_install
     etc.install "agilescan.conf"
+    # Find a way to start the service
   end
 
   # Homebrew requires tests.
